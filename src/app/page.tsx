ef6e85783bd813948ac88ef7277d958046e844db
imMatheus <matheusmendesbarata03@gmail.com>
2025-01-19 17:55:59 -0800

    added spotify sdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <main className="p-4">hej</main>
+import { spotifyApi } from '@/lib/spotify-sdk'
+
+export default async function Home() {
+  const { artists } = await spotifyApi.search('coldplay', ['artist'], 'SE', 11)
+  return (
+    <main className="p-4">
+      <pre>{JSON.stringify(artists, null, 2)}</pre>
+    </main>
+  )
 }
